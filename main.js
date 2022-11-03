85ea03d9f8545d5948941a708dd68a6cc4243dab
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2022-11-03 08:20:31 -0700

    This is the commit for the JS Objects Chapter

--- main.js
+++ main.js
@@ -1,42 +1,75 @@
-// console.log("connected");
-// let sum = (a, b) => a + b;
+// // console.log("connected");
+// // let sum = (a, b) => a + b;
 
-// console.log(sum(5, 4));
+// // console.log(sum(5, 4));
 
-// let divBy3 = (x) => {
-//   if (!(x % 3)) {
-//     return x / 3;
-//   } else {
-//     return "Sorry this number is not divisible by three.";
-//   }
+// // let divBy3 = (x) => {
+// //   if (!(x % 3)) {
+// //     return x / 3;
+// //   } else {
+// //     return "Sorry this number is not divisible by three.";
+// //   }
+// // };
+
+// // console.log(divBy3(5));
+// // console.log(divBy3(6));
+
+// // let factor = (a, b, c) => {
+// //   let factors = [];
+// //   let i = 1;
+// //   console.log(Math.min(a, b, c));
+// //   while (i <= Math.min(a, b, c)) {
+// //     if (a % i === 0 && b % i === 0 && c % i === 0) {
+// //       factors.push(i);
+// //     }
+// //     i++;
+// //   }
+// //   return factors;
+// // };
+
+// // console.log(factor(5, 10, 15));
+// // console.log(factor(9, 27, 30));
+
+// // let sum3 = (x, y) => {
+// //   if (x === y) {
+// //     return x * 3;
+// //   } else {
+// //     return x + y;
+// //   }
+// // };
+
+// // console.log(sum3(2, 3));
+// // console.log(sum3(3, 3));
+
+// let myCar = {
+//   make: "Nissan",
+//   model: "300z",
+//   year: 1995,
+//   color: "black",
+//   "last owner": "John Gripshover",
+
+//   description: function () {
+//     return `${this.make} ${this.model} ${this.year}`;
+//   },
 // };
 
-// console.log(divBy3(5));
-// console.log(divBy3(6));
+// myCar.city = "Oakland";
+// myCar["new owner"] = "Aron Roy";
+// myCar["years sold"] = [2002, 2005, 2010, 2015];
 
-// let factor = (a, b, c) => {
-//   let factors = [];
-//   let i = 1;
-//   console.log(Math.min(a, b, c));
-//   while (i <= Math.min(a, b, c)) {
-//     if (a % i === 0 && b % i === 0 && c % i === 0) {
-//       factors.push(i);
-//     }
-//     i++;
-//   }
-//   return factors;
-// };
+// let whereIsIt = myCar.city;
+// console.log(myCar.description());
 
-// console.log(factor(5, 10, 15));
-// console.log(factor(9, 27, 30));
-
-let sum3 = (x, y) => {
-  if (x === y) {
-    return x * 3;
-  } else {
-    return x + y;
-  }
+let person1 = {
+  name: "Blake",
+  title: "Chief of Design",
+  quote: "Aron is the best!",
 };
 
-console.log(sum3(2, 3));
-console.log(sum3(3, 3));
+let person2 = {
+  name: "Kevin",
+  title: "Chief of People",
+  quote: "I second whatever Blake said!",
+};
+
+console.log(person1, person2);
